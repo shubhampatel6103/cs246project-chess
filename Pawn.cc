61995abf1217f98620bc306b4469076235d7abcb
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-26 15:20:18 -0500

    update Pawn.cc

--- Pawn.cc
+++ Pawn.cc
@@ -2,5 +2,19 @@
 
 Pawn::Pawn(): Piece('p', 1), firstMove{true} {};
 
+void Pawn::attachToCells(Board& b) {
+    if (firstMove) {
+        if (this->getColour() == Colour::Black) {
 
+        } else {
+
+        }
+    } else {
+        if (this->getColour() == Colour::Black) {
+
+        } else {
+            
+        }
+    }
+}
 
