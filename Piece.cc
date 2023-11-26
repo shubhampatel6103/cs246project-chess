cc856cdc6a2796062b4ab055141e4a8db66c88ed
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-26 01:06:40 -0500

    update Queen.cc

--- Piece.cc
+++ Piece.cc
@@ -1,6 +1,6 @@
 #include "Piece.h"
 
-Piece::Piece(): row{0}, col{0}, type{''}, colour{Colour::Black} {} // Initialize the row, column, type and colour of the piece
+Piece::Piece(char type, int points): type{type}, colour{Colour::Black}, row{0}, col{0}, points{points} {} // Initialize the row, column, type and colour of the piece
 
 char Piece::getType() { return type; }
 
@@ -13,3 +13,10 @@ int Piece::getCol() { return col; }
 void Piece::setRow(int r) { row = r; }
 
 void Piece::setCol(int c) { col = c; }
+
+void Piece::notify(Cell& c) {
+    this->attachToCells(Board& b);
+}
+
+
+
--- Piece.h
+++ Piece.h
@@ -2,14 +2,16 @@
 #include "Cell.h"
 #include "Board.h"
 
+
 class Piece : public Observer {
     enum class Colour {Black=0, White};
-    Colour colour;
+
     char type;
-    int row, col;
+    Colour colour;
+    int row, col, points;
 
     public:
-        Piece(); // Initializes the object 
+        Piece(char type, int points); // Initializes the object 
         char getType(); // Returns the type of piece ('k' for king, 'n' for knight and so on)
         Colour getColour(); // Returns the colour of the piece (Black or White)
         int getRow(); // Returns the row at which the piece is located on the board
@@ -17,6 +19,6 @@ class Piece : public Observer {
         void setRow(int r); // Sets the row to r
         void setCol(int c); // Sets the col to c
 
+        virtual void attachToCells(Board& b) = 0; // attaches the piece to the cells required
         void notify(Cell& c); // Calls attachToCells on the piece ... we dont even need this
-        virtual void attachToCells(Board& b) = 0; // attaches 
 }
