542d9b5274e35134bf3f1b915c7fc6a8698526af
rdeep <rdeep@uwaterloo.ca>
2023-11-28 12:54:05 -0500

    Fixed Textdisplay

--- textdisplay.h
+++ textdisplay.h
@@ -7,22 +7,20 @@
 class Cell;
 
 class TextDisplay: public Observer {
-  std::vector<std::vector<char>> theDisplay {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}, 
-                                             {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                                             {'6', ' ', '_', ' ', '_', ' ', '_', ' ', '_'},
-                                             {'5', '_', ' ', '_', ' ', '_', ' ', '_', ' '},
-                                             {'4', ' ', '_', ' ', '_', ' ', '_', ' ', '_'},
-                                             {'3', '_', ' ', '_', ' ', '_', ' ', '_', ' '},
-                                             {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                                             {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                                             {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                                             {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
+  std::vector<std::vector<char>> theDisplay {{'8', ' ', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}, 
+                                             {'7', ' ','p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                                             {'6', ' ',' ', '_', ' ', '_', ' ', '_', ' ', '_'},
+                                             {'5', ' ','_', ' ', '_', ' ', '_', ' ', '_', ' '},
+                                             {'4', ' ',' ', '_', ' ', '_', ' ', '_', ' ', '_'},
+                                             {'3', ' ','_', ' ', '_', ' ', '_', ' ', '_', ' '},
+                                             {'2', ' ','P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                                             {'1', ' ','R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                                             {' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                                             {' ', ' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
  public:
   TextDisplay();
 
   void notify(Cell &c) override;
-  SubscriptionType subType() override;
-
   ~TextDisplay();
 
   friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
