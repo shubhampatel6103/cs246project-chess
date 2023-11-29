34aa0984afd821698b7041a9fa10aec48bfaa1f0
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-29 15:54:16 -0500

    not accessing private fields in Bishop.cc anymore

--- Bishop.cc
+++ Bishop.cc
@@ -11,59 +11,59 @@ void Bishop::notify(Cell &c, Board &b) {
 void Bishop::attachToCells(Board& b) {
 
     // The following is for South - East Diagonal
-    int r = row + 1;
-    int c = col + 1; // now r and c are a diagonal bottom - right to the piece
+    int r = getRow() + 1;
+    int c = getCol() + 1; // now r and c are a diagonal bottom - right to the piece
     while (r < 8 && c < 8) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].attach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).attach(this);
             r++; 
             c++; // Ensures that we are moving forward in the SouthEast Direction
         } else { 
-            b.board[r][c].attach(this);
+            b.getCellAt(r,c).attach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER ATTACHING
     }
 
 
     // The following is for North - West Diagonal
-    int r = row - 1;
-    int c = col - 1; // now r and c are a diagonal top - left to the piece
+    int r = getRow() - 1;
+    int c = getCol() - 1; // now r and c are a diagonal top - left to the piece
     while (r >= 0 && c >= 0) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].attach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).attach(this);
             r--; 
             c--; // Ensures that we are moving forward in the NorthWest Direction
         } else { 
-            b.board[r][c].attach(this);
+            b.getCellAt(r,c).attach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER ATTACHING
     }
 
 
     // The following is for South - West Diagonal
-    int r = row + 1; 
-    int c = col - 1; // now r and c are a diagonal bottom - left to the piece
+    int r = getRow() + 1; 
+    int c = getCol() - 1; // now r and c are a diagonal bottom - left to the piece
     while (r < 8 && c >= 0) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].attach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).attach(this);
             r++; 
             c--; // Ensures that we are moving forward in the SouthWest Direction
         } else { 
-            b.board[r][c].attach(this);
+            b.getCellAt(r,c).attach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER ATTACHING
     }
 
     // The following is for North - East Diagonal
-    int r = row - 1; 
-    int c = col + 1; // now r and c are a diagonal top - right to the piece
+    int r = getRow() - 1; 
+    int c = getCol() + 1; // now r and c are a diagonal top - right to the piece
     while (r >= 0 && c < 8) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].attach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).attach(this);
             r--; 
             c++; // Ensures that we are moving forward in the NorthEast Direction
         } else { 
-            b.board[r][c].attach(this);
+            b.getCellAt(r,c).attach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER ATTACHING
     }
@@ -73,59 +73,59 @@ void Bishop::attachToCells(Board& b) {
 void Bishop::detachFromCells(Board& b) {
 
     // The following is for South - East Diagonal
-    int r = row + 1;
-    int c = col + 1; // now r and c are a diagonal bottom - right to the piece
+    int r = getRow() + 1;
+    int c = getCol() + 1; // now r and c are a diagonal bottom - right to the piece
     while (r < 8 && c < 8) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].detach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).detach(this);
             r++; 
             c++; // Ensures that we are moving forward in the SouthEast Direction
         } else { 
-            b.board[r][c].detach(this);
+            b.getCellAt(r,c).detach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER DETACHING
     }
 
 
     // The following is for North - West Diagonal
-    int r = row - 1;
-    int c = col - 1; // now r and c are a diagonal top - left to the piece
+    int r = getRow() - 1;
+    int c = getCol() - 1; // now r and c are a diagonal top - left to the piece
     while (r >= 0 && c >= 0) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].detach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).detach(this);
             r--; 
             c--; // Ensures that we are moving forward in the NorthWest Direction
         } else { 
-            b.board[r][c].detach(this);
+            b.getCellAt(r,c).detach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER DETACHING
     }
 
 
     // The following is for South - West Diagonal
-    int r = row + 1; 
-    int c = col - 1; // now r and c are a diagonal bottom - left to the piece
+    int r = getRow() + 1; 
+    int c = getCol() - 1; // now r and c are a diagonal bottom - left to the piece
     while (r < 8 && c >= 0) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].detach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).detach(this);
             r++; 
             c--; // Ensures that we are moving forward in the SouthWest Direction
         } else { 
-            b.board[r][c].detach(this);
+            b.getCellAt(r,c).detach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER DETACHING
     }
 
     // The following is for North - East Diagonal
-    int r = row - 1; 
-    int c = col + 1; // now r and c are a diagonal top - right to the piece
+    int r = getRow() - 1; 
+    int c = getCol() + 1; // now r and c are a diagonal top - right to the piece
     while (r >= 0 && c < 8) {
-        if (!b.board[r][c].getPiece()) {
-            b.board[r][c].detach(this);
+        if (!b.getCellAt(r,c).getPiece()) {
+            b.getCellAt(r,c).detach(this);
             r--; 
             c++; // Ensures that we are moving forward in the NorthEast Direction
         } else { 
-            b.board[r][c].detach(this);
+            b.getCellAt(r,c).detach(this);
             break;
         } // Otherwise, we are being blocked so we break AFTER DETACHING
     }
