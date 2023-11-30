259d2af1dfb0f27dd058371cb2f5aaae3a331905
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-30 13:57:35 -0500

    changed detach for queen

--- Queen.cc
+++ Queen.cc
@@ -123,42 +123,22 @@ void Queen::detachFromCells(Board& b) {
 
     // The following for loop takes care of vertical cells forward
     for (int r = getRow() + 1; r < 8; r++) { // start from where we are + 1 to the end of the getCellAt( in terms of rows (getRow() + 1 so we dont check for our own cell)
-        if (!b.getCellAt(r, getCol()).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(r, getCol()).detach(std::make_unique<Queen>(myType, myColour));
-        } else { 
-            b.getCellAt(r, getCol()).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER ATTACHING
+        b.getCellAt(r, getCol()).detach(std::make_unique<Queen>(myType, myColour));
     }
 
     // The following for loop takes care of vertical cells backward
     for (int r = getRow() - 1; r >= 0; r--) { // start from where we are + 1 to the beginning of the getCellAt( in terms of rows (getRow() - 1 1 so we dont check for our own cell)
-        if (!b.getCellAt(r, getCol()).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(r, getCol()).detach(std::make_unique<Queen>(myType, myColour));
-        } else { 
-            b.getCellAt(r, getCol()).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER ATTACHING
+        b.getCellAt(r, getCol()).detach(std::make_unique<Queen>(myType, myColour));
     }
 
     // The following for loop takes care of all horitonzal cells forward
     for (int c = getCol() + 1; getCol() < 8; c++) { // start from where we are + 1 to the end of the getCellAt( in terms of cols (getCol() + 1 so we dont check for our own cell)
-        if (!b.getCellAt(getRow(), c).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(getRow(), c).detach(std::make_unique<Queen>(myType, myColour));
-        } else { 
-            b.getCellAt(getRow(), c).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(getRow(), c).detach(std::make_unique<Queen>(myType, myColour));
     }
 
     // The following for loop takes care of all horitonzal cells backward
     for (int c = getCol() - 1; getCol() >= 0; c--) { // start from where we are - 1 to the beginning of the getCellAt( in terms of cols (getCol() - 1 so we dont check for our own cell)
-        if (!b.getCellAt(getRow(), c).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(getRow(), c).detach(std::make_unique<Queen>(myType, myColour));
-        } else { 
-            b.getCellAt(getRow(), c).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(getRow(), c).detach(std::make_unique<Queen>(myType, myColour));
     }
 
 
@@ -168,14 +148,9 @@ void Queen::detachFromCells(Board& b) {
     int r = getRow() + 1;
     int c = getCol() + 1; // now r and c are a diagonal bottom - right to the piece
     while (r < 8 && c < 8) {
-        if (!b.getCellAt(r, c).getPiece()) {
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            r++; 
-            c++; // Ensures that we are moving forward in the SouthEast Direction
-        } else { 
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
+        r++; 
+        c++; // Ensures that we are moving forward in the SouthEast Direction
     }
 
 
@@ -183,14 +158,9 @@ void Queen::detachFromCells(Board& b) {
     r = getRow() - 1;
     c = getCol() - 1; // now r and c are a diagonal top - left to the piece
     while (r >= 0 && c >= 0) {
-        if (!b.getCellAt(r, c).getPiece()) {
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            r--; 
-            c--; // Ensures that we are moving forward in the NorthWest Direction
-        } else { 
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
+        r--; 
+        c--; // Ensures that we are moving forward in the NorthWest Direction
     }
 
 
@@ -198,28 +168,18 @@ void Queen::detachFromCells(Board& b) {
     r = getRow() + 1; 
     c = getCol() - 1; // now r and c are a diagonal bottom - left to the piece
     while (r < 8 && c >= 0) {
-        if (!b.getCellAt(r, c).getPiece()) {
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            r++; 
-            c--; // Ensures that we are moving forward in the SouthWest Direction
-        } else { 
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
+        r++; 
+        c--; // Ensures that we are moving forward in the SouthWest Direction
     }
 
     // The following is for North - East Diagonal
     r = getRow() - 1; 
     c = getCol() + 1; // now r and c are a diagonal top - right to the piece
     while (r >= 0 && c < 8) {
-        if (!b.getCellAt(r, c).getPiece()) {
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            r--; 
-            c++; // Ensures that we are moving forward in the NorthEast Direction
-        } else { 
-            b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r, c).detach(std::make_unique<Queen>(myType, myColour));
+        r--; 
+        c++; // Ensures that we are moving forward in the NorthEast Direction
     }
 
     // Finally, we are done!
