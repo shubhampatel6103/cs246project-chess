6c85accc0966bc25249fd489c3eb697d7a9b7232
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-30 16:37:23 -0500

    changed game

--- Game.cc
+++ Game.cc
@@ -10,23 +10,24 @@ void Game::printBoard() {
     cout << b << endl;
 }
 
-void Game::makeMove(int arr[4], Piece* p) {
-        b.getCellAt(arr[0],arr[1]).remPiece(); // This is where we were (Source)
+void Game::makeMove(Move& m, Piece* p) {
+        b.getCellAt(m.getCoords()[0], m.getCoords()[1]).remPiece(); // This is where we were (Source)
 
-    if (b.getCellAt(arr[2],arr[3]).hasPiece()) { // This is where we went (Desitnation)
-        b.getCellAt(arr[2],arr[3]).remPiece(); // Remove the piece that already exists
-        b.getCellAt(arr[2],arr[3]).addPiece(p); // Add the piece that we moved
+    if (b.getCellAt(m.getCoords()[2],m.getCoords()[3]).hasPiece()) { // This is where we went (Desitnation)
+        b.getCellAt(m.getCoords()[2],m.getCoords()[3]).remPiece(); // Remove the piece that already exists
+        b.getCellAt(m.getCoords()[2],m.getCoords()[3]).addPiece(p); // Add the piece that we moved
     } else {
-        b.getCellAt(arr[2],arr[3]).addPiece(p);
+        b.getCellAt(m.getCoords()[2],m.getCoords()[3]).addPiece(p);
     }
 }
 
 void Game::playMove(int p) {
     if (p == 1) { // play the move on behalf of p1 if p = 1
-        makeMove(p1->playMove(), p1);
+        p1->move(b);
+        makeMove(p1->getMove(), p1);
     } 
     else { // else, play the move on behalf of p2
-        makeMove(p2->playMove(), p2);
+        makeMove(p2->move(b), p2);
     } // Might need to add try excepts, ideally should be handled by player class
 }
 
--- Game.h
+++ Game.h
@@ -1,19 +1,20 @@
 #ifndef __GAME_H__
 #define __GAME_H__
-//#include "Player.h"
+#include "Player.h"
 #include "Board.h"
+#include "Move.h"
 
 class Game {
-    //Player* p1;
-    //Player* p2;
+    Player* p1;
+    Player* p2;
     Board& b;
 
     public:
-        //Game(Player* p1, Player* p2, Board& b);
+        Game(Player* p1, Player* p2, Board& b);
         ~Game();
 
         void printBoard(); // prints the board to stdout
-        void makeMove(int arr[4], Piece* p); // modifies the board state
+        void makeMove(Move& m, Piece* p); // modifies the board state
         void playMove(int p); // plays the move on behalf of the player
 };
 
