2f88b03e9a7877ca6164347a0c85a448383b560c
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-30 14:01:04 -0500

    changed detach for Rook

--- Rook.cc
+++ Rook.cc
@@ -61,42 +61,22 @@ void Rook::detachFromCells(Board& b) {
 
     // The following for loop takes care of vertical cells forward
     for (int r = getRow() + 1; r < 8; r++) { // start from where we are + 1 to the end of the board in terms of getRow()s (getRow() + 1 so we dont check for our own cell)
-        if (!b.getCellAt(r,getCol()).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(r,getCol()).detach(std::make_unique<Rook>(myType, myColour));
-        } else { 
-            b.getCellAt(r,getCol()).detach(std::make_unique<Rook>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r,getCol()).detach(std::make_unique<Rook>(myType, myColour));
     }
 
     // The following for loop takes care of vertical cells backward
     for (int r = getRow() - 1; r >= 0; r--) { // start from where we are + 1 to the beginning of the board in terms of getRow()s (getRow() - 1 1 so we dont check for our own cell)
-        if (!b.getCellAt(r,getCol()).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(r,getCol()).detach(std::make_unique<Rook>(myType, myColour));
-        } else { 
-            b.getCellAt(r,getCol()).detach(std::make_unique<Rook>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r,getCol()).detach(std::make_unique<Rook>(myType, myColour));
     }
 
     // The following for loop takes care of all horitonzal cells forward
     for (int c = getCol() + 1; getCol() < 8; c++) { // start from where we are + 1 to the end of the board in terms of cols (getCol() + 1 so we dont check for our own cell)
-        if (!b.getCellAt(getRow(),c).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(getRow(),c).detach(std::make_unique<Rook>(myType, myColour));
-        } else { 
-            b.getCellAt(getRow(),c).detach(std::make_unique<Rook>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(getRow(),c).detach(std::make_unique<Rook>(myType, myColour));
     }
 
     // The following for loop takes care of all horitonzal cells backward
     for (int c = getCol() - 1; getCol() >= 0; c--) { // start from where we are - 1 to the beginning of the board in terms of cols (getCol() - 1 so we dont check for our own cell)
-        if (!b.getCellAt(getRow(),c).getPiece()) { // If there is no piece on that cell, then attach myself to that cell
-            b.getCellAt(getRow(),c).detach(std::make_unique<Rook>(myType, myColour));
-        } else { 
-            b.getCellAt(getRow(),c).detach(std::make_unique<Rook>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(getRow(),c).detach(std::make_unique<Rook>(myType, myColour));
     }
     
 }
