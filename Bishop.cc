843e292d077470ae258fbb39ca8de0db406ed765
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-30 13:43:02 -0500

    changed detach for bishop

--- Bishop.cc
+++ Bishop.cc
@@ -82,14 +82,9 @@ void Bishop::detachFromCells(Board& b) {
     int r = getRow() + 1;
     int c = getCol() + 1; // now r and c are a diagonal bottom - right to the piece
     while (r < 8 && c < 8) {
-        if (!b.getCellAt(r,c).getPiece()) {
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            r++; 
-            c++; // Ensures that we are moving forward in the SouthEast Direction
-        } else { 
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
+        r++;
+        c++;
     }
 
 
@@ -97,14 +92,9 @@ void Bishop::detachFromCells(Board& b) {
     r = getRow() - 1;
     c = getCol() - 1; // now r and c are a diagonal top - left to the piece
     while (r >= 0 && c >= 0) {
-        if (!b.getCellAt(r,c).getPiece()) {
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            r--; 
-            c--; // Ensures that we are moving forward in the NorthWest Direction
-        } else { 
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
+        r--;
+        c--;
     }
 
 
@@ -112,28 +102,18 @@ void Bishop::detachFromCells(Board& b) {
     r = getRow() + 1; 
     c = getCol() - 1; // now r and c are a diagonal bottom - left to the piece
     while (r < 8 && c >= 0) {
-        if (!b.getCellAt(r,c).getPiece()) {
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            r++; 
-            c--; // Ensures that we are moving forward in the SouthWest Direction
-        } else { 
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
+        r++;
+        c--;
     }
 
     // The following is for North - East Diagonal
     r = getRow() - 1; 
     c = getCol() + 1; // now r and c are a diagonal top - right to the piece
     while (r >= 0 && c < 8) {
-        if (!b.getCellAt(r,c).getPiece()) {
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            r--; 
-            c++; // Ensures that we are moving forward in the NorthEast Direction
-        } else { 
-            b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
-            break;
-        } // Otherwise, we are being blocked so we break AFTER DETACHING
+        b.getCellAt(r,c).detach(std::make_unique<Bishop>(myType, myColour));
+        r--;
+        c++;
     }
     
 }
