c80a92bff905bb1605ae3f4401fce1b9de4e6dfd
Sharang Goel <s4goel@uwaterloo.ca>
2023-12-01 04:42:06 -0500

    update knight.cc and king.cc

--- King.cc
+++ King.cc
@@ -13,29 +13,33 @@ void King::notify(Cell &c, Board &b) {
 void King::attachToCells(Board& b) {
     
     // We just need to check for the 8 cells around the king
-    if (0 <= getRow() + 1 < 8 && 0 <= getCol() + 1 < 8) { b.getCellAt(getRow() + 1, getCol() + 1).attach(this); }
-    if (0 <= getRow() - 1 < 8 && 0 <= getCol() - 1 < 8) { b.getCellAt(getRow() - 1, getCol() - 1).attach(this); }
-    if (0 <= getRow() - 1 < 8 && 0 <= getCol() + 1 < 8) { b.getCellAt(getRow() - 1, getCol() + 1).attach(this); }
-    if (0 <= getRow() + 1 < 8 && 0 <= getCol() - 1 < 8) { b.getCellAt(getRow() + 1, getCol() - 1).attach(this); }
-    if (0 <= getRow() + 1 < 8) { b.getCellAt(getRow() + 1, getCol()).attach(this); }
-    if (0 <= getRow() - 1 < 8) { b.getCellAt(getRow() - 1, getCol()).attach(this); }
-    if (0 <= getCol() - 1 < 8) { b.getCellAt(getRow(), getCol() - 1).attach(this); }
-    if (0 <= getCol() + 1 < 8) { b.getCellAt(getRow(), getCol() + 1).attach(this); }
+    int r = getRow();
+    int c = getCol();
+    if (0 <= r + 1 && r + 1 < 8 && 0 <= c + 1 && c + 1 < 8) { b.getCellAt(r + 1, c + 1).attach(this); }
+    if (0 <= r - 1 && r - 1 < 8 && 0 <= c - 1 && c - 1 < 8) { b.getCellAt(r - 1, c - 1).attach(this); }
+    if (0 <= r - 1 && r - 1 < 8 && 0 <= c + 1 && c + 1 < 8) { b.getCellAt(r - 1, c + 1).attach(this); }
+    if (0 <= r + 1 && r + 1 < 8 && 0 <= c - 1 && c - 1 < 8) { b.getCellAt(r + 1, c - 1).attach(this); }
+    if (0 <= r + 1 && r + 1 < 8) { b.getCellAt(r + 1, c).attach(this); }
+    if (0 <= r - 1 && r - 1 < 8) { b.getCellAt(r - 1, c).attach(this); }
+    if (0 <= c - 1 && c - 1 < 8) { b.getCellAt(r, c - 1).attach(this); }
+    if (0 <= c + 1 && c + 1 < 8) { b.getCellAt(r, c + 1).attach(this); }
 
 }
 
 void King::detachFromCells(Board& b) {
     
     // We just need to detach from the 8 cells around the king
-    char myType = getType();
-    Colour myColour = getColour();
-    if (0 <= getRow() + 1 < 8 && 0 <= getCol() + 1 < 8) { b.getCellAt(getRow() + 1, getCol() + 1).detach(this); }
-    if (0 <= getRow() - 1 < 8 && 0 <= getCol() - 1 < 8) { b.getCellAt(getRow() - 1, getCol() - 1).detach(this); }
-    if (0 <= getRow() - 1 < 8 && 0 <= getCol() + 1 < 8) { b.getCellAt(getRow() - 1, getCol() + 1).detach(this); }
-    if (0 <= getRow() + 1 < 8 && 0 <= getCol() - 1 < 8) { b.getCellAt(getRow() + 1, getCol() - 1).detach(this); }
-    if (0 <= getRow() + 1 < 8) { b.getCellAt(getRow() + 1, getCol()).detach(this); }
-    if (0 <= getRow() - 1 < 8) { b.getCellAt(getRow() - 1, getCol()).detach(this); }
-    if (0 <= getCol() - 1 < 8) { b.getCellAt(getRow(), getCol() - 1).detach(this); }
-    if (0 <= getCol() + 1 < 8) { b.getCellAt(getRow(), getCol() + 1).detach(this); }
+    // char myType = getType();
+    // Colour myColour = getColour();
+    int r = getRow();
+    int c = getCol();
+    if (0 <= r + 1 && r + 1 < 8 && 0 <= c + 1 && c + 1 < 8) { b.getCellAt(r + 1, c + 1).detach(this); }
+    if (0 <= r - 1 && r - 1 < 8 && 0 <= c - 1 && c - 1 < 8) { b.getCellAt(r - 1, c - 1).detach(this); }
+    if (0 <= r - 1 && r - 1 < 8 && 0 <= c + 1 && c + 1 < 8) { b.getCellAt(r - 1, c + 1).detach(this); }
+    if (0 <= r + 1 && r + 1 < 8 && 0 <= c - 1 && c - 1 < 8) { b.getCellAt(r + 1, c - 1).detach(this); }
+    if (0 <= r + 1 && r + 1 < 8) { b.getCellAt(r + 1, c).detach(this); }
+    if (0 <= r - 1 && r - 1 < 8) { b.getCellAt(r - 1, c).detach(this); }
+    if (0 <= c - 1 && c - 1 < 8) { b.getCellAt(r, c - 1).detach(this); }
+    if (0 <= c + 1 && c + 1 < 8) { b.getCellAt(r, c + 1).detach(this); }
 
 }
