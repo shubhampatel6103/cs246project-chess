c621bde44f62792b4f89aee1b8d3fd015106ef11
Sharang Goel <s4goel@uwaterloo.ca>
2023-11-30 22:37:55 -0500

    update knight.cc

--- Knight.cc
+++ Knight.cc
@@ -51,4 +51,42 @@ void Knight::attachToCells(Board& b) {
 
 }
 
-void Knight::detachFromCells(Board& b) {return;}
+void Knight::detachFromCells(Board& b) {
+    // As a knight, there are only 8 possible moves
+
+    // Case 1
+    int r = getRow() - 2; // Two steps forward
+    int c = getCol() + 1; // One step right
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+
+    // Case 2
+    c = getCol() - 1; // One step left
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+
+    // Case 3
+    r = getRow() + 2; // Two steps back
+    c = getCol() + 1; // One step right
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+
+    // Case 4
+    c = getCol() - 1; // One step left
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+
+    // Case 5
+    c = getCol() - 2; // Two steps left
+    r = getRow() + 1; // One step back
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+
+    // Case 6
+    r = getRow() - 1; // One step forward
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+
+    // Case 7
+    c = getCol() + 2; // Two steps right
+    r = getRow() + 1; // One step back
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+
+    // Case 8
+    r = getRow() - 1; // One step foward
+    if (0 <= r < 8 && 0 <= c <= 8) { b.getCellAt(r,c).detach(this); }
+}
