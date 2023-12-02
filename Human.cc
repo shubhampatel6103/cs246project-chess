8e7fb373ac2c69ec476acb97c1060a37b73d6ae5
sbpatel <sbpatel@uwaterloo.ca>
2023-12-02 06:28:14 -0500

    Got a bug idk how to fix

--- Human.cc
+++ Human.cc
@@ -11,43 +11,39 @@ Human::Human(Colour c): Player{c} {}
 
 void Human::move(Board &b) {
   string s;
-  while (true) {
-    if ((cin >> s)) {
-      if (s == "move" || s == "resign") {
-        break;
-      } else {
-        cout << "Invalid command: Try Again" << endl;
+  while (cin >> s) {
+    if (s == "resign") {
+      m.setMove(true, -1, -1, -1, -1);
+      return;
+    }
+    if (s == "move") {
+      char sourceChar;
+      char destChar;
+      int sourceInt;
+      int destInt;
+      while (true) {
+        if (!(cin >> sourceChar) || !(cin >> sourceInt) || !(cin >> destChar) || !(cin >> destInt)) {
+          cout << "Invalid move command: Try Again" << endl;
+          break;
+        } else {
+          int sX = sourceChar - 'a';
+          int sY = 8 - sourceInt;
+          int dX = destChar - 'a';
+          int dY = 8 - destInt;
+          if (sX > 7 || sX < 0 || sY > 7 || sY < 0 || dX > 7 || dX < 0 || dY > 7 || dY < 0) {
+            continue;
+          }
+          //cout << b.getCellAt(sY, sX).hasPiece(); // Always prints 0 even though it has piece
+                                                    // Keep happening even in other parts of the code
+          if (b.getCellAt(sY, sX).hasPiece() && b.getCellAt(sY, sX).getPiece()->getColour() == id 
+              && b.getCellAt(dY, dX).isPieceObserver(b.getCellAt(sY, sX).getPiece())) {
+            m.setMove(false, sY, sX, dY, dX);
+            return;
+          } 
+        }
       }
     } else {
       cout << "Invalid command: Try Again" << endl;
-    }
-  }
-
-  if (s == "resign") {
-    m.setMove(true, -1, -1, -1, -1);
-    return;
-  }
-
-  char sourceChar;
-  char destChar;
-  int sourceInt;
-  int destInt;
-  while (true) {
-    if (!(cin >> sourceChar) || !(cin >> sourceInt) || !(cin >> destChar) || !(cin >> destInt)) {
-      cout << "Invalid move command: Try Again" << endl;
-    } else {
-      int sX = sourceChar - 97;
-      int sY = sourceInt - 1;
-      int dX = sourceChar - 97;
-      int dY = destInt - 1;
-      if (sX > 7 || sX < 0 || sY > 7 || sY < 0 || dX > 7 || dX < 0 || dY > 7 || dY < 0) {
-        continue;
-      }
-      if (b.getCellAt(sX, sY).hasPiece() && b.getCellAt(sX, sY).getPiece()->getColour() == id 
-          && b.getCellAt(dX, dY).isPieceObserver(b.getCellAt(sX, sY).getPiece())) {
-        m.setMove(false, sX, sY, dX, dY);
-        return;
-      } 
     }
   }
 }
